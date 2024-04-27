48c41fcaf1df76d2aa58549aa09c9ce057c2d8a0
Shanifer Seit <shanifer@gmail.com>
2024-04-26 18:05:23 -0700

    Remove commented-out codez

--- app.py
+++ app.py
@@ -275,18 +275,9 @@ def display_time_range_picker():
                                         captions=["Find open courts from [Start Time] to [End Time]",
                                                   "Find open courts for [Duration] starting at [Start Time]"],
                                         horizontal=True)
-        # if end_time_or_duration == "End Time":
-        #     end_time = st.time_input("End Time", closing_time, step=timedelta(minutes=30))
-        #     end_datetime = to_pst_datetime(end_time)
-        # else:
-        #     duration = st.selectbox("Duration (in hours)", options=get_duration_options(), index=None)
-        #     if duration:
-        #         end_datetime = start_datetime + timedelta(hours=duration)
 
     with col1:
         st.write("Start Time")
-        # start_time = st.time_input("Start Time", opening_time, step=timedelta(minutes=30))
-        # start_datetime = to_pst_datetime(start_time)
         start_hour_col, start_min_col, start_am_pm_col = st.columns(3)
         with start_hour_col:
             start_hour = st.number_input("Hour", min_value=1, max_value=12, value=opening_time.hour, key="start_hour")
